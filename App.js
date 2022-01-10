4b36d2134f0649a31d6864ba47b248ca6920f425
varsharajavel <96564028+varsharajavel@users.noreply.github.com>
2022-01-10 17:21:26 +0530

    TicTacToe using reactjs

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './TicTacToegame/TicTacToe';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
